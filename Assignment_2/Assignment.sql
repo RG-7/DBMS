aeb14af03990e1a7c77d014c858f598daec21e68
Ratn Govindam <88680132+RG-7@users.noreply.github.com>
2024-01-19 11:55:59 +0530

    Update Assignment.sql

--- Assignment_2/Assignment.sql
+++ Assignment_2/Assignment.sql
@@ -40,3 +40,28 @@ SELECT Ename, Job FROM Emp WHERE Job IN('Salesperson','Clerk');
 /*
   Q4) Display all details of employees whose salary between 2000 and 3000 ?
 */
+SELECT * FROM Emp WHERE Salary >2000 AND Salary <3000;
+
+/*
+    Q5) Display all details of employees whose dept no is 10, 20, or 30 ?
+*/
+SELECT * FROM Emp WHERE DeptNo IN(10,20,30);
+
+/*
+    Q6) Display name of those employees whose commission is NULL?
+*/
+SELECT Ename FROM Emp WHERE Commission IS NULL;
+
+/*
+    Q7) Display dept no & salary in ascending order of dept no and within each dept no salary should be in descending order ?
+*/
+SELECT DeptNo, Salary FROM Emp ORDER BY DeptNo ASC, Salary DESC;
+
+/*
+    Q8) Display name of employees having two ‘a’ or ‘A’ chars in the name ?
+*/
+SELECT Ename FROM Emp WHERE Ename LIKE '%A%' OR Ename LIKE '%a%';
+
+/*
+    Q9) Display the name of the employees whose second char is ‘b’ or ‘B’ ?
+*/
